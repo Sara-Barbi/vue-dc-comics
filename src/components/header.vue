0d3653fdca9e7b9544669bc16fa72a92abe2ca30
Sara-Barbi <barbisara02@gmail.com>
2022-01-28 12:36:09 +0100

    try white

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,51 +1,101 @@
 <template>
-  <div class="hello">
-    <div class="logo">
 
-      <img src="../assets/vue-dc-comics-1/img/dc-logo-bg.png" alt="">
+<div class="hello">
+  <div class="container-small d-flex align-content-center my-line-height">
+
+    <div class="logo">
+      <img src="../assets/vue-dc-comics-1/img/dc-logo.png" alt="">
     </div>
     <nav>
       <ul>
-        <li v-for="(element, index)in links" :key="index">
-
-        </li>
+          <li v-for="(element, index) in links" :key="index">
+            <a href="#" :class="{'active': element.current}">{{element.text}}</a>
+          </li>
       </ul>
     </nav>
-    
+  
   </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'Header',
-  
-}
-data(){
-  navbar:[
+name: 'Header',
+
+
+data() {
+return{
+links:[
+  { text: "Home",
+    url: "#",
+    current:"false",
+  },
       
-  ]
+  { text: "Contattaci",
+    url: "#",
+    current:"false",
+  },
+
+  { text: "Cosa Offriamo",
+    url: "#",
+    current:"false",
+  },
+
+  { text: "I nostri prodotti",
+    url: "#",
+    current:"false",
+  },
+
+  { text: "Home",
+    url: "#",
+    current:"false",
+  },
+]
+}
+}
 }
 </script>
 
 
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+
+<style scoped >
+.container-small{
+max-width: 1100px;
+margin: 0 auto;
+}
+.my-line-height{
+line-height: 70px;
+justify-content: space-between;
+}
 h3 {
-  margin: 40px 0 0;
+margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+list-style-type: none;
+padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+display: inline-block;
+margin: 0 10px;
 }
 a {
-  color: #42b983;
+color: #42b983;
 }
 img{
 width: 70px;
 }
 </style>
+
+
+
+
+
+
+
+
+
+
+
+
