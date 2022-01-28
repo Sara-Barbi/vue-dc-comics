984240c5192f6e0781eb1672d6792773ff74ce35
Sara-Barbi <barbisara02@gmail.com>
2022-01-28 13:01:50 +0100

    see some

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,21 @@
     <Header />
     <Main />
     <Footer />
+
+    
+    
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue';
-import Main from './components/Main.vue';
-import Footer from './components/Footer.vue';
+
+import Header from "./components/Header.vue"
+import Main from "./components/Main.vue"
+import Footer from "./components/Footer.vue"
+
+
+
+
 
 
 export default {
@@ -26,31 +34,4 @@ export default {
 
 
 
-</style>
-
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
